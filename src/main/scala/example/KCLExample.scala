649deb155702dd2c9cae4a25be24cf0e95b5b34d
Junichi Sakamoto <sakamoto.jun05@gmail.com>
2020-01-05 14:26:29 +0900

    Source,Flow,Sink をRunnableGraph として動作させる

--- src/main/scala/example/KCLExample.scala
+++ src/main/scala/example/KCLExample.scala
@@ -1,11 +1,16 @@
 import java.net.InetAddress
 import java.util
 import java.util.UUID
+import com.contxt.kinesis.KinesisSource
+import com.contxt.kinesis.KinesisRecord
 
+
+import scala.concurrent.Future
 
 import com.typesafe.config.{ Config, ConfigFactory }
 
 import akka.NotUsed
+import akka.Done
 
 import com.amazonaws.internal.StaticCredentialsProvider
 import com.amazonaws.auth.{BasicAWSCredentials, AWSCredentialsProvider}
@@ -20,13 +25,17 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
 import com.amazonaws.auth.AWSCredentialsProviderChain
-import com.amazonaws.services.kinesis.clientlibrary.lib.worker._
-import com.contxt.kinesis.KinesisSource
+
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.util.Random
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker._
+
 
 object Main extends App{
+
+  import akka.stream.scaladsl._
+
   val config = ConfigFactory.load() 
   val accessKeyId = config.getString("accessKeyId")
   val secretAccessKey = config.getString("secretAccessKey")
@@ -61,9 +70,7 @@ object Main extends App{
     .map { kinesisRecord =>
 
       println(s"[stream] ああああああああ")
-      KeyMessage(
-        kinesisRecord.partitionKey, kinesisRecord.data.utf8String, kinesisRecord.markProcessed
-      )
+      kinesisRecord
     }
     // Records may be processed out of order without affecting checkpointing.
     .grouped(10).map(batch => Random.shuffle(batch)).mapConcat(identity)
@@ -75,8 +82,22 @@ object Main extends App{
 
   implicit val system = ActorSystem("Main")
   implicit val materializer = ActorMaterializer()
-  atLeastOnceSource.runWith(Sink.foreach(println))
 
-  Thread.sleep(100.seconds.toMillis)
+  val parse: Flow[KinesisRecord, KeyMessage, NotUsed] = 
+    Flow[KinesisRecord].map { kinesisRecord =>
+      println(s"[stream] ああああああああ")
+      KeyMessage(
+        kinesisRecord.partitionKey, kinesisRecord.data.utf8String, kinesisRecord.markProcessed
+      )
+    }
+
+  val sink: Sink[KeyMessage, Future[Done]] = Sink.foreach(println)
+
+  val runnableGraph: RunnableGraph[Future[Done
+  ]] = atLeastOnceSource.via(parse).toMat(sink)(Keep.right)
+
+  runnableGraph.run()
+
+  Thread.sleep(50.seconds.toMillis)
   Await.result(system.terminate(), Duration.Inf)
 }
